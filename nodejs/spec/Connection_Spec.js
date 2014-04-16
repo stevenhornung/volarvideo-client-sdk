c7c1913514335dfa49098585f5138ad60d442707
Steven Hornung <steven.hornung@icloud.com>
2014-04-16 18:56:51 -0400

    start jasmine test suite

---
+++ nodejs/spec/Connection_Spec.js
@@ -0,0 +1,13 @@
+var volar = require("../volar.js");
+var config = require('./test-config.json');
+
+var api_key = config.api_key,
+    secret = config.secret,
+    base_url = config.base_url;
+
+
+describe("connection", function() {
+    it("should be established with valid credentials", function() {
+        
+    })
+})
